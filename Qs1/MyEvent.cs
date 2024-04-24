febc1e2bacb5dce6647c0ddb388697a9f60c9053
KayKiP <167833546+KayKiP@users.noreply.github.com>
2024-04-23 20:44:24 -0400

    Add files via upload

---
+++ Qs1/MyEvent.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Qs1.MyEvent;
+
+namespace Qs1
+{
+    public class MyEvent : EventArgs
+    {
+        public string Msg { get; set; }
+
+        public MyEvent(string answer)
+        {
+            Msg = answer;
+        }
+
+        public delegate void EventHandler(object sender, MyEvent e);
+    }
+
+    public class EventPublisher
+    {
+        public event MyEvent.EventHandler MyEvnt;
+
+        public void RaiseEvent(string message)
+        {            
+            OnEvent(new MyEvent(message));
+        }
+
+        protected virtual void OnEvent(MyEvent e)
+        {
+            MyEvent.EventHandler handler = MyEvnt;
+            handler?.Invoke(this, e);
+        }
+    }
+
+    public class EventHandler
+    {
+        public void HandleEvent(object sender, MyEvent e)
+        {
+            Console.WriteLine("Show The line : " + e.Msg);
+        }
+    }
+}
---
+++ Qs1/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Qs1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {           
+            EventPublisher publisher = new EventPublisher();
+            EventHandler handler = new EventHandler();           
+            publisher.MyEvnt += handler.HandleEvent;            
+            publisher.RaiseEvent("Finally Excecuted!!!!!");
+            Console.ReadLine();
+        }
+    }
+}
